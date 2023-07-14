c5d2d75c53b00beabffe005ffce1dac28f82998c
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-07-14 15:08:14 +0200

    Filter tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./components/Todolist";
 
+
+export type FilterValueType = 'All' | 'Completed' | 'Active';
 const App = () => {
     let [tasks, setTasks] = useState([
         {id: 1, title: "HTML&CSS", isDone: true},
@@ -9,13 +11,26 @@ const App = () => {
         {id: 3, title: "ReactJS", isDone: false}
     ])
 
+    const [filterValue, setFilterValue] = useState('all');
+
     const removeTask = (taskId: number) =>  setTasks(tasks.filter(task => task.id !== taskId));
+    const filterTasks = (filterValue: FilterValueType) => setFilterValue(filterValue);
+
+    let filteredTasks = tasks;
+        if(filterValue === 'Completed') {
+            filteredTasks = tasks.filter(task => task.isDone);
+        }
+        if(filterValue === 'Active') {
+            filteredTasks = tasks.filter(task => !task.isDone);
+        }
+
 
     return (
         <div className="App">
             <Todolist title="Sunday"
-                      tasks={tasks}
+                      tasks={filteredTasks}
                       removeTask={removeTask}
+                      filterTasks={filterTasks}
             />
         </div>
     );
--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -1,16 +1,24 @@
 import React from "react";
+import {FilterValueType} from "../App";
 
 type TodolistPropsType = {
     title: string,
     tasks: TaskPropsType[],
-    removeTask: (taskId:number) => void
+    removeTask: (taskId: number) => void,
+    filterTasks: (filterValue: FilterValueType) => void
 }
 type TaskPropsType = {
     id: number,
     title: string,
     isDone: boolean
 }
-export const Todolist:React.FC<TodolistPropsType> = ({title,tasks,removeTask}) => {
+export const Todolist: React.FC<TodolistPropsType> = (
+    {
+        title,
+        tasks,
+        removeTask,
+        filterTasks
+    }) => {
     return (
         <div>
             <h3>{title}</h3>
@@ -21,18 +29,18 @@ export const Todolist:React.FC<TodolistPropsType> = ({title,tasks,removeTask}) =
             <ul>
                 {tasks.map(t => {
                     return (
-                    <li key={t.id}>
-                        <input type='checkbox' checked={t.isDone}/>
-                        <span>{t.title}</span>
-                        <button onClick={() => removeTask(t.id)}>x</button>
-                    </li>
-                )
+                        <li key={t.id}>
+                            <input type='checkbox' checked={t.isDone}/>
+                            <span>{t.title}</span>
+                            <button onClick={() => removeTask(t.id)}>x</button>
+                        </li>
+                    )
                 })}
             </ul>
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={() => filterTasks('All')}>All</button>
+                <button onClick={() => filterTasks('Active')}>Active</button>
+                <button onClick={() => filterTasks('Completed')}>Completed</button>
             </div>
         </div>
     )
