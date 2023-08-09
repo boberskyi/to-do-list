8252aad997dfed8f549bf70747871d1bbf25addf
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-08-09 08:55:10 +0200

    App styling and change direction of adding todolists

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {TaskType, Todolist} from "./components/Todolist";
 import {v1} from "uuid";
 import {AddItemForm} from "./components/AddItemForm";
+import styled from "styled-components";
 
 
 export type FilterValueType = 'All' | 'Completed' | 'Active';
@@ -12,7 +13,7 @@ type TodolistsType = {
     filter: FilterValueType
 }
 type TasksType = {
-    [key:string]: TaskType[]
+    [key: string]: TaskType[]
 }
 const App = () => {
     let todolistID1 = v1()
@@ -36,63 +37,75 @@ const App = () => {
         ]
     })
 
-    const filterTasks = (tdlId:string, filterValue: FilterValueType) => {
+    const filterTasks = (tdlId: string, filterValue: FilterValueType) => {
         setTodolists(todolists.map(tdl => tdl.id === tdlId ? {...tdl, filter: filterValue} : tdl))
     }
-    const removeTask = (tdlId:string, taskId: string) => {
+    const removeTask = (tdlId: string, taskId: string) => {
         setTasks({...tasks, [tdlId]: tasks[tdlId].filter(task => task.id !== taskId)})
     }
-    const addTask = (tdlId:string, newTitle: string) => {
+    const addTask = (tdlId: string, newTitle: string) => {
         const newTask = {id: v1(), title: newTitle, isDone: false};
         setTasks({...tasks, [tdlId]: [...tasks[tdlId], newTask]});
     }
-    const chnageCheckboxStatus = (tdlId:string, taskId:string) => {
-        setTasks({...tasks, [tdlId]: tasks[tdlId].map(task => task.id === taskId ? {...task, isDone: !task.isDone} : task)})
+    const chnageCheckboxStatus = (tdlId: string, taskId: string) => {
+        setTasks({
+            ...tasks,
+            [tdlId]: tasks[tdlId].map(task => task.id === taskId ? {...task, isDone: !task.isDone} : task)
+        })
     }
-    const removeTodolist = (tdlId:string) => {
+    const removeTodolist = (tdlId: string) => {
         setTodolists(todolists.filter(todolist => todolist.id !== tdlId));
         delete tasks[tdlId];
     }
-    const addTodolist = (newTitle:string) => {
+    const addTodolist = (newTitle: string) => {
         const newTdlId = v1();
-        setTodolists([{id: newTdlId, title: newTitle, filter: 'All'},...todolists]);
+        setTodolists([...todolists, {id: newTdlId, title: newTitle, filter: 'All'}]);
         setTasks({...tasks, [newTdlId]: []})
     }
-    const updateTaskTitle = (tdlId:string, taskId:string, newTitle:string) => {
-         setTasks({...tasks, [tdlId]: tasks[tdlId].map(el => el.id === taskId ? {...el, title: newTitle}: el)});
+    const updateTaskTitle = (tdlId: string, taskId: string, newTitle: string) => {
+        setTasks({...tasks, [tdlId]: tasks[tdlId].map(el => el.id === taskId ? {...el, title: newTitle} : el)});
     }
-    const updateTdlTitle = (tdlId:string, newTitle:string) => {
-        setTodolists(todolists.map(tdl => tdl.id === tdlId ? {...tdl, title: newTitle}: tdl))
+    const updateTdlTitle = (tdlId: string, newTitle: string) => {
+        setTodolists(todolists.map(tdl => tdl.id === tdlId ? {...tdl, title: newTitle} : tdl))
     }
 
     return (
         <div className="App">
-            <AddItemForm callback={(newTitle:string) => addTodolist(newTitle)} />
+            <StyledAppWrapper>
+                {todolists.map(tdl => {
+                    let filteredTasks = tasks[tdl.id];
+                    if (tdl.filter === 'Completed') {
+                        filteredTasks = tasks[tdl.id].filter(task => task.isDone);
+                    }
+                    if (tdl.filter === 'Active') {
+                        filteredTasks = tasks[tdl.id].filter(task => !task.isDone);
+                    }
 
-            {todolists.map(tdl => {
-                let filteredTasks = tasks[tdl.id];
-                if (tdl.filter === 'Completed') {
-                    filteredTasks = tasks[tdl.id].filter(task => task.isDone);
-                }
-                if (tdl.filter === 'Active') {
-                    filteredTasks = tasks[tdl.id].filter(task => !task.isDone);
-                }
+                    return <Todolist title={tdl.title}
+                                     key={tdl.id}
+                                     tdlId={tdl.id}
+                                     tasks={filteredTasks}
+                                     removeTask={removeTask}
+                                     filterTasks={filterTasks}
+                                     addTask={addTask}
+                                     chnageCheckboxStatus={chnageCheckboxStatus}
+                                     removeTodolist={removeTodolist}
+                                     updateTdlTitle={(tdlId: string, newTitle: string) => updateTdlTitle(tdlId, newTitle)}
+                                     updateTaskTitle={(tdlId: string, taskId: string, newTitle: string) => updateTaskTitle(tdlId, taskId, newTitle)}
+                    />
+                })}
 
-                return <Todolist title={tdl.title}
-                                 key={tdl.id}
-                                 tdlId={tdl.id}
-                                 tasks={filteredTasks}
-                                 removeTask={removeTask}
-                                 filterTasks={filterTasks}
-                                 addTask={addTask}
-                                 chnageCheckboxStatus={chnageCheckboxStatus}
-                                 removeTodolist={removeTodolist}
-                                 updateTdlTitle={(tdlId:string, newTitle:string) => updateTdlTitle(tdlId, newTitle)}
-                                 updateTaskTitle={(tdlId:string, taskId:string, newTitle:string) => updateTaskTitle(tdlId, taskId, newTitle)}
-                />
-            })}
+                <AddItemForm callback={(newTitle: string) => addTodolist(newTitle)}/>
+            </StyledAppWrapper>
         </div>
     );
 }
 
 export default App;
+
+const StyledAppWrapper = styled.div`
+  display: flex;
+  gap: 20px;
+  padding: 30px;
+  align-items: flex-start;
+`
