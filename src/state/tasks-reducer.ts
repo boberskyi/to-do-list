b0358e9fede1e464e45e8e2d3209bf5acf63983e
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-08-18 11:19:47 +0200

    Chnage task status reducer

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -1,10 +1,11 @@
 import {TasksType} from "../App";
 import {v1} from "uuid";
 
-type ActionType = RemoveTaskACType | AddTaskACType;
+type ActionType = RemoveTaskACType | AddTaskACType | ChangeTaskStatusACType;
 
 type RemoveTaskACType = ReturnType<typeof removeTaskAC>;
 type AddTaskACType = ReturnType<typeof addTaskAC>;
+type ChangeTaskStatusACType = ReturnType<typeof changeTaskStatusAC>;
 
 
 export const tasksReducer = (state: TasksType, action: ActionType):TasksType => {
@@ -14,6 +15,9 @@ export const tasksReducer = (state: TasksType, action: ActionType):TasksType => 
         }
         case 'ADD-TASK': {
             return {...state, [action.payload.tdlId]: [{id: v1(), title: action.payload.taskTitle, isDone: false}, ...state[action.payload.tdlId]]};
+        }
+        case 'CHANGE-TASK-STATUS': {
+            return {...state, [action.payload.tdlId]: state[action.payload.tdlId].map(task => task.id === action.payload.taskId ? {...task, isDone: action.payload.isDone}: task)}
         }
         default:
             throw new Error('I don\'t understand this type')
@@ -34,6 +38,16 @@ export const addTaskAC = (taskTitle: string, tdlId: string) => {
         type: 'ADD-TASK',
         payload: {
             taskTitle,
+            tdlId
+        }
+    } as const
+}
+export const changeTaskStatusAC = (taskId:string, isDone:boolean, tdlId:string) => {
+    return {
+        type: 'CHANGE-TASK-STATUS',
+        payload: {
+            taskId,
+            isDone,
             tdlId
         }
     } as const
