4530abfa493f7ed9d02d6f72289c7a8ca4ff7864
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-08-24 13:20:43 +0200

    Redux install

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import AppWithReducers from "./AppWithReducers";
+import AppWithRedux from "./AppWithRedux";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <AppWithReducers />
+    <AppWithRedux />
   </React.StrictMode>
 );
 
