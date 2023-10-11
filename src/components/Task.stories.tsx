59d421b358f73a59a4c8caacbde13d53bc9655c2
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-10-11 13:58:53 +0200

    Clicked story checkbox

--- src/components/Task.stories.tsx
+++ src/components/Task.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import {Task} from "./Task";
 import {action} from '@storybook/addon-actions';
+import {useState} from "react";
+import {TaskType} from "./todolist/Todolist";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -14,32 +16,36 @@ const meta = {
     tags: ['autodocs'],
     // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
     argTypes: {
-        // callback: {
-        //     description: 'Btn clicked',
-        //     action: 'clicked'
-        // },
+        removeTask: { action: 'Removed' },
+        updateTaskTitle: { action: 'Updated Title' },
+        chnageCheckboxStatus: { action: 'Changed Checkbox Status' }
     },
+    args: {
+        task: {id: 'string', title: 'Primary', isdone: false},
+        tdlId: 'Primary',
+
+    }
 } satisfies Meta<typeof Task>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
-    args: {
-        task: {id: 'string', title: 'Primary', isdone: false},
-        tdlId: 'Primary',
-        removeTask: action('Removed'),
-        updateTaskTitle: action('Updated Title'),
-        chnageCheckboxStatus: action('Changed Checkbox Status')
-    }
-};
+export const Primary: Story = {};
 export const Checked: Story = {
     args: {
         task: {id: 'string', title: 'Checked', isdone: true},
-        tdlId: 'Checked',
-        removeTask: action('Removed'),
-        updateTaskTitle: action('Updated Title'),
-        chnageCheckboxStatus: action('Changed Checkbox Status')
     }
+};
+const WorkingTask = () => {
+    const [task, setTask] = useState<TaskType>({id: 'string', title: 'Primary', isdone: false});
+
+    return <Task task={task}
+                 tdlId={'string'}
+                 removeTask={action(`Removed task id: ${task.id}`)}
+                 updateTaskTitle={(newTitle: string) => setTask({...task, id: 'newId', title: newTitle})}
+                 chnageCheckboxStatus={() => setTask({...task, isdone: !task.isdone})} />
+}
+export const Working: Story = {
+    render: () => <WorkingTask />
 };
