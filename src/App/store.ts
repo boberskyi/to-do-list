4ef27caa24678b7563a314148b43bd65381ad99f
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-12-02 17:50:14 +0100

    Change Store to RTK standard

--- src/App/store.ts
+++ src/App/store.ts
@@ -1,34 +1,31 @@
-import { TasksActionType, tasksReducer } from "../feautures/Todolists/Task/tasks-reducer";
-import {
-  TodolistsActionType,
-  todolistsReducer,
-} from "../feautures/Todolists/Todolist/todolists-reducer";
-import { AnyAction, applyMiddleware, combineReducers, legacy_createStore } from "redux";
-import thunk, { ThunkAction, ThunkDispatch } from "redux-thunk";
+import { tasksReducer } from "../feautures/Todolists/Task/tasks-reducer";
+import { todolistsReducer } from "../feautures/Todolists/Todolist/todolists-reducer";
+import { AnyAction } from "redux";
+import { ThunkAction, ThunkDispatch } from "redux-thunk";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { appReducer } from "./app-reducer";
 import { authReducer } from "../feautures/Todolists/Login/auth-reducer";
+import { configureStore } from "@reduxjs/toolkit";
 
-const rootReducer = combineReducers({
-  app: appReducer,
-  tasks: tasksReducer,
-  todolists: todolistsReducer,
-  auth: authReducer,
+export const store = configureStore({
+  reducer: {
+    app: appReducer,
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+    auth: authReducer,
+  },
 });
 
-//export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
-
-export type AppRootStateType = ReturnType<typeof rootReducer>;
+// export type AppRootStateType = ReturnType<typeof rootReducer>;
+export type AppRootStateType = ReturnType<typeof store.getState>;
 
 export type AppDispatchType = ThunkDispatch<AppRootStateType, unknown, AnyAction>;
-
-export type AppActionsType = TodolistsActionType | TasksActionType;
 
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   AppRootStateType,
   unknown,
-  AppActionsType
+  AnyAction
 >;
 export const useAppDispatch = useDispatch<AppDispatchType>;
 
