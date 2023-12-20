83834fbc05f679140c9e67fe6936714c2340af5a
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-12-20 20:10:52 +0100

    Change ErrorUtils to RTK

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,29 +1,25 @@
-import {
-  setAppErrorAC,
-  SetAppErrorACType,
-  setAppStatusAC,
-  SetAppStatusACType,
-} from "../App/app-reducer";
 import { Dispatch } from "redux";
 import { ResponseType } from "../todolist-api";
+import { appActions } from "../App/app-reducer";
 
 export const handleServerAppError = <T>(
   dispatch: ErrorUtilsDispatchType,
   data: ResponseType<T>,
 ) => {
   if (data.messages.length) {
-    dispatch(setAppErrorAC(data.messages[0]));
+    dispatch(appActions.setError({ error: data.messages[0] }));
   } else {
-    dispatch(setAppErrorAC("Error"));
+    dispatch(appActions.setError({ error: "Error" }));
   }
-  dispatch(setAppStatusAC("failed"));
+  dispatch(appActions.setStatus({ status: "failed" }));
 };
 export const handleServerNetworkError = (
   dispatch: ErrorUtilsDispatchType,
   e: { message: string },
 ) => {
-  dispatch(setAppErrorAC(e.message));
-  dispatch(setAppStatusAC("failed"));
+  dispatch(appActions.setError({ error: e.message }));
+  dispatch(appActions.setStatus({ status: "failed" }));
 };
 
-type ErrorUtilsDispatchType = Dispatch<SetAppErrorACType | SetAppStatusACType>;
+type ErrorUtilsDispatchType = Dispatch;
+// type ErrorUtilsDispatchType = Dispatch<SetAppErrorACType | SetAppStatusACType>;
